c29a55748223daa1b800a7186ff1811ee14e4369
Onyeami <>
2023-09-07 18:59:43 +0000

    added a quiz html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,12 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     event.preventDefault();
 // Prevent the form from refreshing the page
 const email = document.getElementById("email").value;
+
+// Validate the user input
+if (email) {
+// Store the email in localStorage
+    localStorage.setItem("userEmail", email);
+// Navigate to the quiz screen
+    window.location.href = "quiz.html";
+}
 })
