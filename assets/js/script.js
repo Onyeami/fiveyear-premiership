fa85b8677620e58a13277d149705a0582d606aea
Onyeami <>
2023-09-07 18:00:01 +0000

    applying rules on the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
+// Fuction to display the login form
 function showLoginForm() {
     document.getElementById("login-form").style.display = "block";
     document.getElementById("rules").style.display = "none";
     document.getElementById("scores").style.display = "none";
 }
+
+// Function to display the rules section
 function showRules() {
     document.getElementById("login-form").style.display = "none";
     document.getElementById("rules").style.display = "block";
     document.getElementById("scores").style.display = "none";
 }
 
+// Function to display the scores section
 function showScores() {
     document.getElementById("login-form").style.display = "none";
     document.getElementById("rules").style.display = "none";
     document.getElementById("scores").style.display = "block";
 }
+
+// Retrieve the email from localStorage
+const userEmail = localStorage.getItem("userEmail");
+
+// Retrieve the score associated with the email from localStorage
+const userPoints = localStorage.getItem(userEmail + "_points");
